30b02e3d21a28436afac0aa9773c6e3bcd3248c4
joko susanto <susantojoko809@gmail.com>
2024-12-07 08:01:38 +0700

    welcome

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PegawaiController;
 
-Route::get('/pegawai', function () {
+Route::get('', function () {
     return view('welcome');
 });
 
